d354de417007ac40ded8ba7879340f599cc1fdd6
mxrsel <marsel14082006@gmail.com>
2024-08-15 02:35:48 +0600

    added library's for project and added store with hooks

--- guestbook-frontend/src/main.tsx
+++ guestbook-frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Provider} from 'react-redux'
 import App from './App.tsx'
-import './index.css'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme.ts';
+import { store } from './app/store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
     <App />
-  </StrictMode>,
+    </ThemeProvider>
+  </Provider>,
 )
